1484014a7c52f8c1ea16848df0fcdfc19f92daea
John Urrutia <john@johnurrutia.com>
2023-09-10 13:18:12 -0300

    Communication child - parent with two way data binding [()] and @Output and EventEmitter

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css']
 })
-export class AddCharacterComponent implements OnInit {
+export class AddCharacterComponent {
 
-  constructor() { }
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
-  ngOnInit() {
+  public character: Character = {
+    name: '',
+    power: 0
+  };
+
+  emitCharacter() {
+    console.log(this.character);
+    if (this.character.name === '') return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
   }
-
 }
+
